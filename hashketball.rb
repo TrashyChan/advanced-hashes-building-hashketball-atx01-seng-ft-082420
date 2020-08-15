0c527d9d78608cc01cdc5844d7a2920943399d8f
John Lonetree <jlonetree@gmail.com>
2020-08-15 01:10:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,8 +3,8 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => {
-        :alan_anderson => [
+      :players => [
+        :alan_anderson => {
           :player_name => "Alan Anderson",
           :number => 0,
           :shoe => 16,
@@ -14,7 +14,7 @@ def game_hash
           :steals => 3,
           :blocks => 1,
           :slam_dunks => 1
-        ],
+        },
         :reggie_evans => {
           :player_name => "Reggie Evans",
           :number => 30,
