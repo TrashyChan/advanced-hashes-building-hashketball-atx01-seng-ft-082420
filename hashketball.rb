b17d1eea5573f352f6c366be6291450e74fdbcb6
John Lonetree <jlonetree@gmail.com>
2020-08-15 01:07:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
       :colors => ["Black", "White"],
       :players => [
         :alan_anderson => {
-          :player_name => Alan Anderson,
+          :player_name = "Alan Anderson",
           :number => 0,
           :shoe => 16,
           :points => 22,
